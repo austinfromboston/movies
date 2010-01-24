1aeedda5007f0841be359730dcc9889d80c0550e
Austin Putman <austin@radicaldesigns.org>
2010-01-23 16:03:06 -0800

    tries to talk to nyt

--- app/models/movie-list.js
+++ app/models/movie-list.js
@@ -3,3 +3,12 @@ function MovieList() {
 
 }
 
+MovieList.prototype.import = function( response ) {
+}
+
+MovieList.prototype.getMovies = function( ) {
+	var self = this;
+	new Ajax.Request( Mojo.appInfo.target + Mojo.appInfo.apikey, 
+			{ method: 'get',
+			  onSuccess: self.import } );
+}
--- test/app/models/movie-list-test.js
+++ test/app/models/movie-list-test.js
@@ -1,9 +1,32 @@
 describe( "MovieList", function() {
 	var movielist;
+	var api_url = Mojo.appInfo.target + Mojo.appInfo.apikey;
+
 	beforeEach( function() {
 		movielist = new MovieList();
 	});
 	it( "should have items", function() {
 		expect( movielist.items ).toBeDefined();
 	});
+	//test to make sure that getMovies calls the ajax request correctly
+	it( "should contact nyt movies", function() {
+		spyOn(Ajax, 'Request');
+
+		movielist.getMovies( );
+
+		//movielist.getMovies.mostRecentCall.args
+		
+		expect( Ajax.Request ).wasCalledWith( api_url, { method: 'get', onSuccess: movielist.import  } );
+	});
+
+	
+	//
+	//test to say that the designated success callback will populate the movielist
+	//test to say that the designated error callback will blow up
+
+	//eval(pockets.readFile("test/fixtures/movies.json"));
+
+	//
+	//test movielist populate method
+
 });
